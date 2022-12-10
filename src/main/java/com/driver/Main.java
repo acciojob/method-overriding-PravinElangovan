25f50e72b503737e60aa16ca972a8fdb7d8a7948
PravinElangovan <pravinkumarelangovan20@gmail.com>
2022-12-10 19:03:56 +0530

    Third commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,8 +12,7 @@ public class Main {
       }
   }
   public static void main(String[] args){
-      A a = new A();
-      System.out.println(a.meth());
+
       B b = new B();
       System.out.println(b.meth());
   }
