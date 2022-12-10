7339e8cf8b5f8d0873a182c75be3eac4d4739323
PravinElangovan <pravinkumarelangovan20@gmail.com>
2022-12-10 18:43:16 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+  public static class A{
+      public String meth(){
+          return "Method from class A";
+      }
+  }
+  public static class B extends A{
+      public String meth(){
+          return "Mehtod is Overridden in class B";
+      }
+  }
+  public static void main(String[] args){
+      B b = new B();
+      System.out.print(b.meth());
+  }
 }
